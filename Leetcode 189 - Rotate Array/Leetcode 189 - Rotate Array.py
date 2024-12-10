79eefb0d518530da6c1f5fd9ace35825eb0572d6
Sumit Kumar Nagdeo <compiledtogether@gmail.com>
2024-12-10 19:43:37 +0530

    Create Leetcode 189 - Rotate Array.py

---
+++ Leetcode 189 - Rotate Array/Leetcode 189 - Rotate Array.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/rotate-array
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+        
+# Time Complexity: O(n)
+# Space Complexity: O(1)
