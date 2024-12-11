b4017da52a2b27bc83ba01643dfbb2e5b5a96dee
Sumit Kumar Nagdeo <compiledtogether@gmail.com>
2024-12-11 18:24:50 +0530

    Create Leetcode 6 - Zigzag Conversion.py

---
+++ Arrays-and-Strings/Leetcode 6 - Zigzag Conversion/Leetcode 6 - Zigzag Conversion.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/zigzag-conversion
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        rows = [''] * min(numRows, len(s))
+        curRow, goingDown = 0, False
+
+        for c in s:
+            rows[curRow] += c
+            if curRow == 0 or curRow == numRows - 1:
+                goingDown = not goingDown
+            curRow += 1 if goingDown else -1
+
+        return ''.join(rows)
+    
+# Time complexity: O(n)
+# Space complexity: O(n)
